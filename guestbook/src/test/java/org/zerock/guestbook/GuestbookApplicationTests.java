86a68b314e31fe2ebcc4cccba48c6993a112b4d0
GoRyeoJin <sooon9254@naver.com>
2022-06-06 23:53:34 +0900

    예제 프로젝트를 위한 기본 설정과 querydsl 설정

---
+++ guestbook/src/test/java/org/zerock/guestbook/GuestbookApplicationTests.java
@@ -0,0 +1,14 @@
+package org.zerock.guestbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Hello");
+	}
+
+}
